70d6803828790e927c92d4e7a22f477860e8907b
mqxsyy <gertmagi9@gmail.com>
2025-04-15 10:29:58 +0300

    added wakatime to nvim, some other changes

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -18,15 +18,3 @@ vim.o.relativenumber = true
 vim.diagnostic.config { virtual_text = true }
 
 vim.cmd "set clipboard+=unnamedplus"
-vim.g.clipboard = {
-    name = "WslClipboard",
-    copy = {
-        ["+"] = "clip.exe",
-        ["*"] = "clip.exe",
-    },
-    paste = {
-        ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-        ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    },
-    cache_enabled = 0,
-}
---
+++ packages.md
@@ -0,0 +1,46 @@
+Hyprland
+NetworkManager `$ nmtui`
+
+Package manager:
+- yay (manual install)
+- npm
+- flatpak
+
+Clipboard
+- wlclipboard
+
+Color Palette: 
+- wallust
+
+Wallpaper: 
+- hyprpaper
+
+Status bar: 
+- waybar
+
+Code Editor:
+- nvim
+
+Cli:
+- kitty
+- zsh
+- starship
+
+Browser:
+- zen-browser-bin
+
+Monitoring:
+- nvtop (gpu)
+- btop (general)
+
+Roblox: 
+- vinegar (flatpak)
+- sober (flatpak)
+
+
+
+nvidia drivers, ref hyprland docs
+pipewire-pulse
+
+*create proper install script*
+*create symlink script*

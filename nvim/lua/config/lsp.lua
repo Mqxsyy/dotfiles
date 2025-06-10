52670a1dd37d4012a1668162b7bc931a1f3017cf
mqxsyy <gertmagi9@gmail.com>
2025-06-10 14:42:24 +0300

    changed nvim - actually fixed luau lsp

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -1,36 +1,36 @@
 vim.lsp.enable {
-    "lua_ls",
+	"lua_ls",
 }
 
 vim.diagnostic.config { virtual_text = true }
 
 vim.lsp.config("*", {
-    capabilities = require("lsp").create_capabilities(),
+	capabilities = require("lsp").create_capabilities(),
 })
 
 --> Luau
-vim.lsp.config("luau_lsp", {
-    settings = {
-        ["luau-lsp"] = {
-            completion = {
-                imports = {
-                    enabled = true,
-                    ignoreGlobs = { "**/_Index/**", "**/Packages/**", "**/ServerPackages/**" },
-                },
-            },
-            require = {
-                mode = "relativeToFile",
-            },
-            inlayHints = {
-                functionReturnTypes = true,
-                parameterTypes = true,
-            },
-            ignoreGlobs = { "**/_Index/**", "**/Packages/**", "**/ServerPackages/**" },
-        },
-    },
+vim.lsp.config("luau-lsp", {
+	settings = {
+		["luau-lsp"] = {
+			completion = {
+				imports = {
+					enabled = true,
+					ignoreGlobs = { "**/_Index/**", "**/Packages/**", "**/ServerPackages/**" },
+				},
+			},
+			require = {
+				mode = "relativeToFile",
+			},
+			inlayHints = {
+				functionReturnTypes = true,
+				parameterTypes = true,
+			},
+			ignoreGlobs = { "**/_Index/**", "**/Packages/**", "**/ServerPackages/**" },
+		},
+	},
 })
 
 --> Command to view Lsp logs
 vim.api.nvim_create_user_command("LspLog", function()
-    vim.cmd("edit " .. vim.lsp.get_log_path())
+	vim.cmd("edit " .. vim.lsp.get_log_path())
 end, {})

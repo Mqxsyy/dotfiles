8a66e7c40c8234acc4974734b808904c9287601f
mqxsyy <gertmagi9@gmail.com>
2025-07-05 11:17:19 +0300

    tmux changes, forced vesktop and zen to launch on start, in workspaces 1 and 2

---
+++ tmux/sessions/project-hustle.sh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+
+SESSION_NAME="project-hustle"
+
+if tmux has-session -t $SESSION_NAME 2>/dev/null; then
+    echo "Session already exists!"
+    tmux attach-session -t $SESSION_NAME
+    exit 0
+fi
+
+echo "Creating session..."
+
+tmux new-session -d -s "$SESSION_NAME" -n "nvim"
+tmux send-keys -t "$SESSION_NAME:1" "cd ~/Projects/project-hustle && clear && nvim" C-m
+
+tmux new-window -t "$SESSION_NAME:2" -n "rojo" 
+tmux send-keys -t "$SESSION_NAME:2" "cd ~/Projects/project-hustle && clear && rojo serve" C-m
+
+tmux new-window -t "$SESSION_NAME:3" -n "commands" 
+tmux send-keys -t "$SESSION_NAME:3" "cd ~/Projects/project-hustle && ./bin/build.sh && clear" C-m
+
+tmux select-window -t "$SESSION_NAME:1"
+tmux attach-session -t "$SESSION_NAME"
+

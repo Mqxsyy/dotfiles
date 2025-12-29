c15df6ae0d556c0319820904d1943c6215cac4e3
mqxsyy <gertmagi9@gmail.com>
2025-12-29 19:36:14 +0200

    tiny changes to luau, added custom gdscript formatter and linter

--- nvim/lua/plugins/languages/luau.lua
+++ nvim/lua/plugins/languages/luau.lua
@@ -1,41 +1,42 @@
 local function rojo_project()
-    return vim.fs.root(0, function(name)
-        return name:match "%.project.json$"
-    end)
+	return vim.fs.root(0, function(name)
+		return name:match "%.project.json$"
+	end)
 end
 
 if rojo_project() then
-    vim.filetype.add {
-        extension = {
-            lua = function(path)
-                return path:match ".nvim.lua$" and "lua" or "luau"
-            end,
-        },
-    }
+	vim.filetype.add {
+		extension = {
+			lua = function(path)
+				return path:match ".nvim.lua$" and "lua" or "luau"
+			end,
+		},
+	}
 end
 
 return {
-    "lopi-py/luau-lsp.nvim",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
-    config = function()
-        require("luau-lsp").setup {
-            platform = {
-                type = "roblox",
-            },
-            sourcemap = {
-                enabled = true,
-                autogenerate = true,
-                rojo_project_file = "default.project.json",
-            },
-            types = {
-                roblox_security_level = "PluginSecurity",
-            },
-            fflags = {
-                sync = true,
+	"lopi-py/luau-lsp.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	config = function()
+		require("luau-lsp").setup {
+			platform = {
+				type = "roblox",
+			},
+			sourcemap = {
+				enabled = true,
+				autogenerate = true,
+				rojo_project_file = "default.project.json",
+			},
+			types = {
+				roblox_security_level = "PluginSecurity",
+			},
+			fflags = {
+				sync = true,
 				enable_new_solver = true,
-            },
-        }
-    end,
+				enableByDefault = true,
+			},
+		}
+	end,
 }
--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -5,11 +5,47 @@ return {
 	config = function()
 		local null_ls = require "null-ls"
 
+		local helpers = require "null-ls.helpers"
+		local methods = require "null-ls.methods"
+
+		local FORMATTING = methods.internal.FORMATTING
+		local DIAGNOSTICS = methods.internal.DIAGNOSTICS
+
 		local sources = {
 			null_ls.builtins.formatting.stylua,
 
-			null_ls.builtins.formatting.gdformat,
-			null_ls.builtins.diagnostics.gdlint,
+			-- gdscript format
+			helpers.make_builtin {
+				factory = helpers.formatter_factory,
+				filetypes = { "gd", "gdscript" },
+				generator_opts = {
+					command = "gdscript-formatter",
+					args = { "--safe" },
+					to_stdin = true,
+				},
+				method = { FORMATTING },
+			},
+
+			-- gdscript lint
+			helpers.make_builtin {
+				factory = helpers.generator_factory,
+				filetypes = { "gd", "gdscript" },
+				generator_opts = {
+					command = "gdscript-formatter",
+					args = { "lint", "$FILENAME" },
+					to_stdin = false,
+					from_stderr = true,
+					to_temp_file = true,
+					format = "line",
+					on_output = helpers.diagnostics.from_patterns {
+						{
+							pattern = [[^.+:(%d+):[^:]+:[^:]+: (.+)$]],
+							groups = { "row", "message" },
+						},
+					},
+				},
+				method = DIAGNOSTICS,
+			},
 		}
 
 		if findPrettierConfig() then

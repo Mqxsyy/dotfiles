3c79c80e5ae95d7fd1d35b7d62823e289d53e432
mqxsyy <gertmagi9@gmail.com>
2026-01-04 13:30:09 +0200

    cleaned cmp

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -4,28 +4,17 @@ return {
 	{
 		"hrsh7th/cmp-nvim-lsp",
 	},
-	-- {
-	-- 	"L3MON4D3/LuaSnip",
-	-- 	dependencies = {
-	-- 		"saadparwaiz1/cmp_luasnip",
-	-- 		"rafamadriz/friendly-snippets",
-	-- 	},
-	-- 	config = function()
-	-- 		require("luasnip").filetype_extend("luau", { "lua" })
-	-- 	end,
-	-- },
 	{
 		"hrsh7th/nvim-cmp",
 		config = function()
 			local cmp = require "cmp"
-			-- require("luasnip.loaders.from_vscode").lazy_load()
 
 			cmp.setup {
-				-- snippet = {
-				-- 	expand = function(args)
-				-- 		require("luasnip").lsp_expand(args.body)
-				-- 	end,
-				-- },
+				snippet = {
+					expand = function(args)
+						vim.snippet.expand(args.body)
+					end,
+				},
 				window = {
 					completion = cmp.config.window.bordered(),
 					documentation = cmp.config.window.bordered(),
@@ -38,7 +27,6 @@ return {
 				},
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
-					-- { name = "luasnip" },
 				}, {
 					{ name = "buffer" },
 				}),
